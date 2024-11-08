b62534dd52581117af644db603233bcfea303069
冰水不冰 <icewaternotice@gmail.com>
2024-11-08 18:14:00 +0800

    Auto commit from Unity Asset Bundle Builder

--- Assets/Editor/AssetBundleBuilder.cs
+++ Assets/Editor/AssetBundleBuilder.cs
@@ -39,7 +39,7 @@ public class AssetBundleBuilder
 
     private static void CommitAndPushToGit()
     {
-        RunGitCommand("git add .");
+        RunGitCommand("git add Assets/*");
         RunGitCommand("git commit -m 'Auto commit from Unity Asset Bundle Builder'");
         RunGitCommand("git push origin main");
     }
